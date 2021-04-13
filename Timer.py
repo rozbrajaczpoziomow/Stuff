d22d8a8dd24ea03b7e89bf877c2eca07ac70bf5d
rozbrajaczpoziomow <58668179+rozbrajaczpoziomow@users.noreply.github.com>
2021-04-13 14:45:36 +0200

    Code improvements (Exclusive to index.js)

--- Timer.py
+++ Timer.py
@@ -1,22 +1,26 @@
 from colorama import init as colors, Fore; colors()
 from threading import Thread
 from time import sleep
+from random import choice
 
 def worker():
 	# Timer Thread
+	Colors = [Fore.BLACK, Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.YELLOW]
+	Colors = ([''] * (20//5)) + [choice(Colors) for _ in range(60//5)]
 	step = 1
 	time = -step
-	while not sleep(1):
+	while not sleep(.01):
 		time += step
 		mins = time // 60
-		if mins >= 60: print(Fore.MAGENTA + 'Limit hit'); exit()
+		if mins >= 60: print('\n' + Fore.MAGENTA + ' Limit hit'); exit()
 		secs = time  % 60
 		if   mins == 0                   : color = Fore.LIGHTGREEN_EX
 		elif mins in [1, 2, 3, 4]        : color = Fore.GREEN
 		elif mins in [5, 6, 7, 8, 9]     : color = Fore.YELLOW
 		elif mins in [10, 11, 12, 13, 14]: color = Fore.LIGHTRED_EX
-		else: color = Fore.RED
-		print(f'  {color}{str(mins).zfill(2)}:{str(secs).zfill(2)}{Fore.RESET}')
+		elif mins in [15, 16, 17, 18, 19]: color = Fore.RED
+		else                             : color = Colors[mins//5]
+		print(f'\r  {color}{str(mins).zfill(2)}:{str(secs).zfill(2)}{Fore.RESET}', end='', flush=True)
 
 def stop():
 	# Thread to stop the timer: when this thread exits, the main program dies, and tada!
